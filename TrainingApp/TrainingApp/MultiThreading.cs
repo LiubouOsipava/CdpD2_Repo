ea23f1ecd59e15edecac17639576dea79bad904f
Liubou_Osipava <Liubou_Osipava@EPBYBREW0074>
2018-10-08 17:32:07 +0300

    Add Training trial file

--- TrainingApp/TrainingApp/MultiThreading.cs
+++ TrainingApp/TrainingApp/MultiThreading.cs
@@ -6,26 +6,26 @@ namespace TrainingApp
 {
     class MultiThreading
     {
-        static void Main(string[] args)
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                ThreadPool.QueueUserWorkItem(CallBack, i);
-            }
+        //static void Main(string[] args)
+        //{
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        ThreadPool.QueueUserWorkItem(CallBack, i);
+        //    }
 
-            Console.ReadKey();
-            Thread.Sleep(1000);
+        //    Console.ReadKey();
+        //    Thread.Sleep(1000);
 
-            for (int i = 0; i < 10; i++)
-            {
-                Task.Factory.StartNew(CallBack, i);
-            }
-            Console.ReadKey();
-        }
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        Task.Factory.StartNew(CallBack, i);
+        //    }
+        //    Console.ReadKey();
+        //}
 
-        static void CallBack(object state)
-        {
-            Console.WriteLine($"Iteration: {(int)state}, Thread ID: {Thread.CurrentThread.ManagedThreadId}");
-        }
+        //static void CallBack(object state)
+        //{
+        //    Console.WriteLine($"Iteration: {(int)state}, Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+        //}
     }
 }
